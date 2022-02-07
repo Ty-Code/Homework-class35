880be7d0fe262b3aa61bd2e2a619e43be102eba3
Talha Y <tycode0@gmail.com>
2022-02-07 20:23:54 +0100

    updated ex-4

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -3,11 +3,8 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 function rollDice() {
   return new Promise((resolve) => {
     const dice = [1, 2, 3, 4, 5];
-    const promises = dice.map((die) => {
-      return new Promise((resolve) => {
-        resolve(rollDie(die));
-      });
-    });
+    const promises = dice.map((die) => rollDie(die));
+
     resolve(Promise.all(promises));
   });
 }
@@ -17,8 +14,6 @@ function main() {
     .then((results) => console.log('Resolved!', results))
     .catch((error) => console.log('Rejected!', error.message));
 }
-
-main();
 
 // ! Do not change or remove the code below
 if (process.env.NODE_ENV !== 'test') {
