dfaea553e24d7f546105d405aee9555473fdd116
Talha Y <tycode0@gmail.com>
2022-02-15 18:35:51 +0100

    comleted all

--- 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
+++ 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
@@ -51,7 +51,6 @@ async function fetchAndRender() {
     renderLaureates(laureates);
   } catch (err) {
     console.error(`Something went wrong: ${err.message}`);
-    console.log(err.stack);
   }
 }
 
